6dbf79886e94dff01be9539a6acc137f5c839e0c
merantauw <zholu.vlad2010@yandex.ru>
2022-01-30 11:29:07 +0500

    finished header

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,21 @@
 $(function () {
-  
-})
+  // icon search
+  $("#search").on('click', function () {
+    $(".menu-item").addClass('hide-item');
+    $(".header__search-form").addClass('active');
+    $(".cross").addClass('active');
+    $("#search").hide();
+  })
+  $(".cross").on('click', function () {
+    $(".menu-item").removeClass('hide-item');
+    $(".header__search-form").removeClass('active');
+    $(".cross").removeClass('active');
+    $("#search").show();
+  })
+
+  // sticky scroll header
+  window.addEventListener("scroll", function () {
+    const header = this.document.querySelector(".header__top-inner");
+    header.classList.toggle("sticky", window.scrollY > 0)
+  })
+});
